bfc74711c0ca5d99c82e4c05253dee93becf12e5
Jan Henke <mail@janhenke.dev>
2025-08-19 23:26:22 +0200

    Create the Apple multiplatform project

---
+++ xOS/RetirementDays/RetirementDays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RetirementDays
+//
+//  Created by Jan Henke on 19.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xOS/RetirementDays/RetirementDays/RetirementDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetirementDaysApp.swift
+//  RetirementDays
+//
+//  Created by Jan Henke on 19.08.25.
+//
+
+import SwiftUI
+
+@main
+struct RetirementDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
